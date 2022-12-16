29fdf33b2274007d93453cc28b98fc751ba1b51c
Sahilkumar26244 <kumarsahil8981@gmail.com>
2022-12-16 20:34:27 +0530

    second project start

---
+++ second-project/src/App.js
@@ -0,0 +1,16 @@
+import { Button } from '@chakra-ui/react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+function App() {
+  return (
+  <Router>
+    <Routes>
+      <Route/>
+    </Routes>
+  </Router>
+
+  )
+  
+}
+
+export default App;
