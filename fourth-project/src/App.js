d75607207eedd16df4277a6ecc601024e1dd8317
Sahilkumar26244 <kumarsahil8981@gmail.com>
2023-01-28 11:29:14 +0530

    fourth project

---
+++ fourth-project/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import "./styles/app.scss"
+import {BrowserRouter ,Routes,Route} from 'react-router-dom'
+import Home from './components/Home';
+import Header from './components/Header';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+   <BrowserRouter>
+   <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/cart' element={<Cart/>} />
+    </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
